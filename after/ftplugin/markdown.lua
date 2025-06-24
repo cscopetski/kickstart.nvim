87a6c241b416bbc5463a5bab445a88cd898dfdb1
Caleb Scopetski <cscopetski@gmail.com>
2025-06-24 19:12:43 -0400

    Add obsidian.nvim plugin

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Set conceal level for markdown files
+-- Used for obsidian.nvim plugin
+-- https://github.com/epwalsh/obsidian.nvim/issues/286#issuecomment-1877391540
+vim.opt_local.conceallevel = 2
